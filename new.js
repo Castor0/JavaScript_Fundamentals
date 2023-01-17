462ef99f176ef130de908fbb32df6888a5c88f60
Castor <gregsmarv@gmail.com>
2023-01-17 05:03:20 +0000

    day2

--- new.js
+++ new.js
@@ -66,7 +66,7 @@ console.log(info('vandal'));
 
 
 
-//Rest Operator
+
 
 
 const mSum = (num1, num2) => console.log(num1 + num2);
@@ -80,8 +80,58 @@ const nSum = (...args) => {
 for(let i=0; i<args.length; i++){
     sum += args[i]
 }
-    console.log(sum)
+    console.log(sum);
 };
 
 nSum(2,4,6,8,);
 
+// Spread Operator
+console.clear();
+
+let mArr1 = [1,2,3,4,5];
+
+console.log([...mArr1]);
+
+// using spread opereator to assing values 
+ let nArr1 = [3,4,7,8,9];
+ let nArr2 = [...nArr1];
+
+ // i always clear cosole beecause of earlier logs
+ console.clear();
+ console.log(nArr2);
+
+//changing the value of the spread operator 
+
+let gArr1 =  [2,3,6,7,7,];
+let gArr2 = [...gArr1];
+ 
+// gArr2 retains the initial values of gArr1
+gArr1.push(9);
+console.log(gArr1)
+console.log(gArr2);
+
+
+// Concatinating multiple arrays 
+console.clear();
+
+let fArr1 = [2,4,5,6];
+let fArr2 = [7,8,9];
+
+let fArr3 = [...fArr1, ...fArr2];// the sequence or the arrangement of the arrays matter 
+
+console.log(fArr3);
+
+
+// Using spread operator with objects 
+console.clear()
+let mObj1 = {
+    fName: 'Julius'
+}
+
+let mObj2 = {
+    age : 34
+}
+
+let mObj3 = {...mObj1, ...mObj2}
+
+console.log(mObj3);
